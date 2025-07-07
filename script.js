47daed8009a418b0d6a0a12e6ca4fe21cd5157ac
Farrukh(Fara) Dilshodov <farrukh.dilshodov@gmail.com>
2025-07-07 13:28:09 -0400

    Added calculation

--- script.js
+++ script.js
@@ -27,19 +27,33 @@ function addDecimal() {
     }
 }
 
+// Calculate first and second values depending on operator
+const calculate = {
+    '/': (firstNumber, secondNumber) => firstNumber / secondNumber,
+    '*': (firstNumber, secondNumber) => firstNumber * secondNumber,
+    '+': (firstNumber, secondNumber) => firstNumber + secondNumber,
+    '-': (firstNumber, secondNumber) => firstNumber - secondNumber,
+    '=': (firstNumber, secondNumber) => secondNumber
+};
+
 function useOperator(operator) {
     const currentValue = Number(calculatorDisplay.textContent);
+    // Prevent multiple operators
+    if (operatorValue && awaitingNextValue) {
+        operatorValue = operator;
+        return;
+    }
     // Assign firstValue if no value
     if (!firstValue) {
         firstValue = currentValue;
     } else {
-        console.log('currentValue:', currentValue, 'firstValue:', firstValue, 'operatorValue:', operatorValue);
+        const calculation = calculate[operatorValue](firstValue, currentValue);
+        calculatorDisplay.textContent = calculation;
+        firstValue = calculation;        
     }
     // Ready for next value, store operator
     awaitingNextValue = true;
     operatorValue = operator;
-    console.log('firstValue:', firstValue, 'operatorValue:', operatorValue, 'currentValue:', currentValue);
-    
 }
 
 // Add Event Listeners for numbers, operators, decimal buttons
